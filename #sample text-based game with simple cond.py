8dcd05f858aab209bbef4b5dcbb16fa6c8596c58
KevsKevs <91657106+KevsKevs@users.noreply.github.com>
2021-09-30 15:12:15 +0800

    Add files via upload

--- #sample text-based game with simple cond.py
+++ #sample text-based game with simple cond.py
@@ -1,8 +1,8 @@
+import random
 #sample text-based game with simple conditional statement
-from typing import KeysView
-
-
 character_health = 100
+monster_health = 100
+character_power= 25
 item = ""
 
 name = input("Enter your name: ")
@@ -16,7 +16,7 @@ while character_health > 0:
             print("Your character's health is back to " + str(character_health))
         else :
              print("Your character's health is back to " + str(character_health))
-
+        
 
     v = int(input("Choose 1 if you want to cross the river\nChoose 2 if you want to jump on the ravine\nChoose 3 if you want to fight monster in the dungeon \nInput: "))
    
@@ -42,13 +42,42 @@ while character_health > 0:
         print("You have jumped into the ravine")
         print("Your character has taken 10pt of damage")
         character_health = character_health - 10
-    elif v == 3:
-        #damages the player
-        print("You have enter the dungeon")
 
-    else:
-        print("Invalid input")
+    elif v == 3:
+        #fighting the monster
+        print("You have enter the dungeon")
+        answer = input("Which weapon do you want to use to defeat the monster? [1 for Magic Templar Blade/ 2 for Lava Katana]")
+        if answer == "1":    
+            print("You choose and use the magic templar blade on monster")
+            monster_health = monster_health - 50
+            print("The Ice Dragon didn't die and ate you")
+            print("The monster's Health: " + str(monster_health))
+            
+        elif answer == "2":
+            print("You choose and use the Lava Catana on monster")
+            monster_health = monster_health - 100
+            print("The Ice Dragon is dead " + str(monster_health))
+            print("You kill the Ice Dragon and you obtain his power ")
+            character_power = character_power + 1000
+            print("Your character's Power increase by: " + str(character_power))
+            character_health = character_health - 0
+           
+            another = input("what you gonna do next after you defeat the dragon [1 Exit to the dungeon/ 2 Go deep down inside dungeon]")
+            if another == "1":
+                print("you are already out of the dungeon")
+            elif another == "2":
+                print("You saw another monster")
+                print("The monster is what they called HYDRA")
+            second = input("Do you want to fight the mighty HYDRA? [1 YES/ 2 run away")
+            if second == "1":
+                print("you fight the monster and you die")
+                character_health = character_health - 100
+                print("Your Power is too weak to defeat HYDRA")
+            else :
+                print("You run away but monster hit you")
+                print("You slighty injured")
+                character_health = character_health - 30
 
     print("Your character's Health: " + str(character_health))
-
-print("Your Character is dead!")
+    
+print("Game Over")
